1898e70c49f0e5927a32cf073e7bc22eb03e7f0d
ananth2 <ananthram.p@tresbu.com>
2018-12-21 18:07:02 +0530

    commiting into master with arraylist

--- src/main/java/utilpackage/utillearn.java
+++ src/main/java/utilpackage/utillearn.java
@@ -1,5 +1,23 @@
 package utilpackage;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.annotations.Test;
+
 public class utillearn {
+	
+	@Test
+	public void arraytest1(){
+		
+		List<String>l=new ArrayList<String>();
+		l.add("ananth");
+		l.add("kavitha");
+		l.add("abhiram");
+		
+		System.out.println(l);
+		
+		
+	}
 
 }
