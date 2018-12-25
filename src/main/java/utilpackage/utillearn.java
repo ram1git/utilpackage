f4d25bc86c208467c9a482470cd8eff392851434
ananth2 <ananthram.p@tresbu.com>
2018-12-25 20:09:27 +0530

    adding hash set hash map tree set

--- src/main/java/utilpackage/utillearn.java
+++ src/main/java/utilpackage/utillearn.java
@@ -1,14 +1,18 @@
 package utilpackage;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.testng.annotations.Test;
 
 public class utillearn {
 	
-	@Test
+	@Test(priority=1)
 	public void arraytest1(){
 		
 		List<String>l=new ArrayList<String>();
@@ -22,7 +26,7 @@ public class utillearn {
 	}
 	
 	
-	@Test
+	@Test(priority=2)
 	
 	
 	public void linkedlist(){
@@ -36,5 +40,106 @@ public class utillearn {
 System.out.println(l2);
 		
 	}
+	
+	
+	
+	@Test(priority=3)
+	
+	
+	public void hashSet(){
+		
+		Set<String>set=new HashSet<String>();
+		set.add("ajmer");
+		set.add("delhi");
+		set.add("alampur");
+		set.add("aligarh");
+		set.add("bangalore");
+		set.add("cement nagar");
+		set.add("chennai");
+		
+		
+		HashSet<String>set1=new HashSet<String>();
+		
+		set1.add("ajmer1");
+		set1.add("delhi1");
+		set1.add("alampur1");
+		set1.add("aligarh1");
+		set1.add("bangalore1");
+		set1.add("cement nagar1");
+		set1.add("chennai1");
+		
+		
+		set.addAll(set1);
+		
+		System.out.println("the value of set is -----"+set);
+		
+		
+		LinkedHashSet<String> set2=new LinkedHashSet<String>();
+		
+		set2.add("ajmer");
+		set2.add("delhi");
+		set2.add("alampur");
+		set2.add("aligarh");
+		set2.add("bangalore");
+		set2.add("cement nagar");
+		set2.add("chennai");
+		
+		LinkedHashSet<String> set3=new LinkedHashSet<String>();
+		
+		
+		set3.add("ajmer1");
+		set3.add("delhi1");
+		set3.add("alampur1");
+		set3.add("aligarh1");
+		set3.add("bangalore1");
+		set3.add("cement nagar1");
+		set3.add("chennai1");
+		
+		
+		set2.addAll(set3);
+		
+		System.out.println(set2);
+		
+		
+		
+		
+		}
+		
+		
+	
+	@Test(priority=5)
+	
+	public void treeset(){
+		
+		TreeSet<String>set2=new TreeSet<String>();
+		set2.add("ajmer");
+		set2.add("delhi");
+		set2.add("alampur");
+		set2.add("aligarh");
+		set2.add("bangalore");
+		set2.add("cement nagar");
+		set2.add("chennai");
+		
+		
+		System.out.println("sorted tree set is ---"+set2);
+		
+		
+		
+		TreeSet<String>set3=new TreeSet<String>();
+		set3.add("ajmer1");
+		set3.add("delhi1");
+		set3.add("alampur1");
+		set3.add("aligarh1");
+		set3.add("bangalore1");
+		set3.add("cement nagar1");
+		set3.add("chennai1");
+		
+		set2.addAll(set3);
+		
+		System.out.println("sorted tree set is ---"+set2);
+		
+		
+	}
+	}
 
-}
+
